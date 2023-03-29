069089cccd9ae25c89b120eb96daf89210baa73b
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-03-29 23:57:30 +0400

    init

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index() {
+        $collection = collect([1, 2, 3, 4, 5]);
+        $average = $collection->averageValue(); // returns 3
+
+       dd($average);
+    }
+}
